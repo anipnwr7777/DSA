48d432524695484f9febfb705afa16d020cdede6
anirudh <anirudhpanwar7777@gmail.com>
2021-07-31 14:05:46 +0530

    new

---
+++ binary_search_tree/find_floor_and_ceil_of_a_key_in_bst.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool flag = false;  // flag to mark whether ceil and floor are found or not
+
+struct Node{
+	int key;
+	Node *left = nullptr, *right = nullptr;
+	Node(int key){
+		this->key = key;
+		this->left = this->right = nullptr;
+	}
+};
+
+Node *insert(Node *root, int data){  // insert the new node and return the final pointer of the final tree
+	if(root == nullptr){
+		return new Node(data);  // create the first node with the data that is given as input and return the pointer to the data
+	}
+	if(data < root->key){
+		root->left = insert(root->left, data);
+	}
+	else if(data > root->key){		// make changes and join the new node to the previously made tree by equating to the pointer.
+		root->right = insert(root->right, data);
+	}
+	else{  // already exist donot do anything just return the root
+		return root;
+	}
+
+	return root;  // return the final pointer.
+}
+
+void findFloorCeil(Node *root, int data, int previous, int n, int curr){
+	if(root == nullptr){		// no tree
+		return;
+	}
+
+	if(curr == n && !flag && root->key < data){  	// last node exceeded
+		cout << "the floor is: " << root->key << endl;
+		cout << "the ceil doesnot exist" <<endl;
+		flag = true;
+	}
+	else if(root->key == data && !flag){	// element found itself
+		cout << "the ceil and floor are both same: " << root->key << endl;
+		flag = true;
+	}
+	else if(root->key > data && !flag){		
+		if(previous == -1)
+			cout << "no floor" << endl;		// first node exceeded
+		else
+			cout << "the floor is: " << previous << endl;		// node not found, print just smaller and just bigger
+		cout << "the ceil is : " << root->key << endl;
+		flag = true;
+	}
+
+	findFloorCeil(root->left, data, root->key, n, curr+1);		
+	findFloorCeil(root->right, data, root->key, n, curr+1);
+}
+
+int main(){
+	vector<int> values; 
+	int n;
+	cin >> n;
+	values.resize(n);
+
+	for(int i=0 ; i<n ; i++)
+		cin >> values[i];
+
+	Node *root = nullptr;
+	for(int i=0 ; i<n ; i++){
+		root = insert(root, values[i]);
+	}
+
+	int data;
+	cin >> data;
+	findFloorCeil(root, data, -1, n, 1);
+	return 0;
+}
---
+++ binary_search_tree/find_kth_smallest_and_kth_largest_element.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int key;
+	Node *left = nullptr, *right = nullptr;
+	Node(int key){
+		this->key = key;
+		this->left = this->right = nullptr;
+	}
+};
+
+Node *insert(Node *root, int data){  // insert the new node and return the final pointer of the final tree
+	if(root == nullptr){
+		return new Node(data);  // create the first node with the data that is given as input and return the pointer to the data
+	}
+	if(data < root->key){
+		root->left = insert(root->left, data);
+	}
+	else if(data > root->key){		// make changes and join the new node to the previously made tree by equating to the pointer.
+		root->right = insert(root->right, data);
+	}
+	else{  // already exist donot do anything just return the root
+		return root;
+	}
+
+	return root;  // return the final pointer.
+}
+
+void printTree(Node *root){  // inorder printing
+	if(root == nullptr)
+		return;
+	printTree(root->left);
+	cout << root->key << " ";
+	printTree(root->right);
+}
+
+void findkthLargest(Node *root, int num, int k){
+	if(root == nullptr)
+		return;
+	if(num == k)
+		cout << "kth largest element is: " << root->key << endl;
+	findkthLargest(root->left, num+1, k);
+	findkthLargest(root->right, num+1, k);
+}
+
+void findkthSmallest(Node *root, int num, int k){
+	if (root == nullptr)
+		return;
+	if(num == k)
+		cout << "kth smallest element is: " << root->key << endl;
+	findkthSmallest(root->left, num+1, k);
+	findkthSmallest(root->right, num+1, k);
+}
+
+int main(){
+	vector<int> values; 
+	int n;
+	cin >> n;
+	values.resize(n);
+
+	for(int i=0 ; i<n ; i++)
+		cin >> values[i];
+
+	Node *root = nullptr;
+	for(int i=0 ; i<n ; i++){
+		root = insert(root, values[i]);
+	}
+
+	int k;
+	cin >> k;
+	findkthSmallest(root, 1, k);
+	findkthLargest(root, 1, n-k+1);
+	return 0;
+}
